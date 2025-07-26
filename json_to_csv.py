fd0764f06fbb06846e91963c31e0e42c3fe66521
hakuyou <35139359+hakuyouhiro@users.noreply.github.com>
2025-07-26 20:34:54 +0800

    Create json_to_csv.py

---
+++ json_to_csv.py
@@ -0,0 +1,22 @@
+import json
+import csv
+_country_fr_full.json
+
+while i < 86:
+  print(i)
+  i += 1
+  with open(f'{i}_country_fr_full.json') as jf:
+      jd = json.load(jf)
+  
+  df = open(f'{i}_country_fr_full.csv', 'w', newline='')
+  cw = csv.writer(df)
+  
+  c = 0
+  for data in jd:
+      if c == 0:
+          header = data.keys()
+          cw.writerow(header)
+          c += 1
+      cw.writerow(data.values())
+  
+  df.close()
