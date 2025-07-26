d0e8e06622376eb1ec1062620a3f933861e39115
hakuyou <35139359+hakuyouhiro@users.noreply.github.com>
2025-07-26 19:50:50 +0800

    Update scrap_wine_data_country.py

--- scrap_wine_data_country.py
+++ scrap_wine_data_country.py
@@ -34,7 +34,7 @@ if __name__ == '__main__':
     #set region id
     #region_id = 385;
     country_codes = "fr"
-
+    
     # Defines the payload, i.e., filters to be used on the search
     payload = {
         "country_codes[]": country_codes,
@@ -63,6 +63,8 @@ if __name__ == '__main__':
     print(f'Number of matches: {n_matches}')
     # Create a wine ID list to check if already inputed
     wine_id_list = []
+    wine_count_added = 0
+    wine_count_skipped = 0
     # Iterates through the amount of possible pages
     for i in range(start_page, max(1, int(n_matches / c.RECORDS_PER_PAGE)) + 1):
         # Creates a dictionary to hold the data
@@ -96,8 +98,10 @@ if __name__ == '__main__':
             # Check if wine_id exists in the wine_id
             if  wine_id in wine_id_list:
                 print(f'         {wine_id}_{winery_id}_{winery_name} already exist. Skipping')
+                wine_count_skipped = wine_count_skipped + 1
             else:
                 print(f'         {wine_id}_{winery_id}_{winery_name} not in list. Exporting')
+                wine_count_added = wine_count_added + 1
                 # Popping redundant values
                 if wine['style']:
                     wine['style'].pop('country', None)
@@ -129,3 +133,6 @@ if __name__ == '__main__':
                 wine_id_list.append(wine["id"])
         # Closes the file
         f.close()
+        print(f'         Wine Added = {wine_count_added}')
+        print(f'         Wine Skipped = {wine_count_skipped}')
+        
